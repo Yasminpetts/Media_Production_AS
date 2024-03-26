0ea0c055ecfc338dffedd1ed96ce8b8bdfe53285
Yasminpetts <yaz.petts@icloud.com>
2024-03-26 10:37:20 +0000

    Weather selection UI
    UI made for buttons to change weather, particle system made for rain affect.

---
+++ Assets/Scripts/Weather.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParticleController : MonoBehaviour
+{
+    public ParticleSystem rainFallParticleSystem; // Reference to your RainFall Particle System
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        // Find the RainFallButton and SunButton by name within the Canvas2
+        Button rainFallButton = GameObject.Find("Canvas2/Panel2/RainFallButton").GetComponent<Button>();
+        Button sunButton = GameObject.Find("Canvas2/Panel2/SunButton").GetComponent<Button>();
+
+        // Add listeners for button clicks
+        rainFallButton.onClick.AddListener(ActivateRainFall);
+        sunButton.onClick.AddListener(DeactivateRainFall);
+
+        //show cursor
+        Cursor.lockState = CursorLockMode.None;
+    }
+    // Function to activate the RainFall particle system
+    public void ActivateRainFall()
+    {
+        rainFallParticleSystem.Play();
+    }
+
+    // Function to deactivate the RainFall particle system
+    public void DeactivateRainFall()
+    {
+        rainFallParticleSystem.Stop();
+    }
+
+    
+}
